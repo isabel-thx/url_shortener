06ecb0e1cd21d8b8cfffa093368498b0fe39daac
isabel-thx <isabelhx.teh@gmail.com>
2017-10-19 16:35:52 +0800

    added new and create actions

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,8 +5,33 @@ class UrlsController < ApplicationController
 	end
 
 	def show
-		byebug
     @url = Url.find(params[:id])
+	end
+
+	def new
+ 		@url = Url.new
+	end
+
+	def create
+  # @url = Url.new(params[:url])
+  @url = Url.new(url_params)
+  @url.shorten
+  	if @url.save
+      redirect_to @url  
+  	else
+      render 'new'
+  	end   
+	end
+
+	def destroy
+    @url = Url.find(params[:id])
+    @url.destroy
+    redirect_to urls_path
+	end
+
+	private 
+	def url_params
+    params.require(:url).permit(:long_url)
 	end
 
 end
